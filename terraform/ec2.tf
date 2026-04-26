98766bf56fadc58ee5a2c67eab9f5b19699eb5ea
Bhavyabgb <bgbhavyashree3@gmail.com>
2026-04-26 20:42:51 +0530

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_key_pair" "deployer" {
   key_name   = "terraform-key"
-  public_key = file("${path.module}/terra-key.pub")
+  public_key = file("${path.module}/terraform-key.pub")
 }
 
 resource "aws_default_vpc" "default" {
